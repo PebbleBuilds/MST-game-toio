a3f538058fa09ae842d0e662483e172b2fa1e59c
PebbleBuilds <rocco.ruan4@gmail.com>
2024-08-09 16:47:39 -0700

    catch the fruit first commit

--- MST-game-toio-30/Assets/Scripts/CatchTheFruit/CTFCubeManager.cs
+++ MST-game-toio-30/Assets/Scripts/CatchTheFruit/CTFCubeManager.cs
@@ -6,8 +6,9 @@ using Cysharp.Threading.Tasks;
 
 public class CTFCubeManager : NetworkBehaviour
 {
+
     public ConnectType connectType = ConnectType.Real; 
-    public int m_playerID = 0;
+    public int m_playerID;
     public int m_numPlayers = 3;
     CubeManager cm;
 
@@ -21,11 +22,15 @@ public class CTFCubeManager : NetworkBehaviour
 
     Vector2 m_playerPosID;
 
-    public int[] m_vibrationArray = new int[m_numPlayers]; 
+    public int[] m_vibrationArray; 
     ToioVibration m_playerVibration = new ToioVibration();
 
     async void Start()
     {
+        m_vibrationArray = new int[m_numPlayers];
+        m_playerID = (int)NetworkManager.Singleton.LocalClientId;
+        m_guiMsg1 = String.Format("Client ID={0})", m_playerID);
+
         // Only try to connect to cubes if this is our PlayerObject.
         if (IsOwner)
         {
@@ -78,18 +83,17 @@ public class CTFCubeManager : NetworkBehaviour
                                 m_vibrationArray[m_playerID] = 0;
                             }
                         }
+
+                        // render necessary vibrations.
+                        int vibrationTotal = 0;
+                        foreach(var val in m_vibrationArray)
+                        {
+                            vibrationTotal += val;
+                        }
+                        m_playerVibration.Vibrate(cm.cubes[m_playerID], vibrationTotal);
                     }
                 }
             }
-
-            // render necessary vibrations.
-            int vibrationTotal = 0;
-            foreach(var val in m_vibrationArray)
-            {
-                vibrationTotal += val;
-            }
-            m_playerVibration.Vibrate(cm.cubes[m_playerID], vibrationTotal);
-
         }
     }
 
@@ -97,7 +101,7 @@ public class CTFCubeManager : NetworkBehaviour
     {
         transform.position = ToioHelpers.PositionIDtoUnity(c.pos.x,c.pos.y);
         transform.eulerAngles = new Vector3(0,c.angle,0);
-        m_guiMsg1 = String.Format("Player pos=(x:{0}, y:{1}, angle:{2})", c.pos.x,c.pos.y,c.angle);
+
 
         m_playerPosID.x = c.pos.x;
         m_playerPosID.y = c.pos.y;
