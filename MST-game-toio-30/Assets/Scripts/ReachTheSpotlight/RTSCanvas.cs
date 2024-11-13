f987af03d1cc40da2f7a9e4f603753272ce900ca
PebbleBuilds <rocco.ruan4@gmail.com>
2024-11-12 16:30:37 -0800

    trying to sync canvas across to client

--- MST-game-toio-30/Assets/Scripts/ReachTheSpotlight/RTSCanvas.cs
+++ MST-game-toio-30/Assets/Scripts/ReachTheSpotlight/RTSCanvas.cs
@@ -1,15 +1,16 @@
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.UI;
+using Unity.Collections;
 using System.Collections;
 
 public class RTSCanvas : NetworkBehaviour
 {
     public Text m_text;
     public Image m_background;
-    string m_textString = "";
+    NetworkVariable<FixedString128Bytes> m_textString = new NetworkVariable<FixedString128Bytes>("", NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
     NetworkVariable<int> m_leaderID = new NetworkVariable<int>(0, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
-    bool m_animating;
+    NetworkVariable<bool> m_animating = new NetworkVariable<bool>(false, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
     int m_playerID;
 
     void Start()
@@ -19,7 +20,7 @@ public class RTSCanvas : NetworkBehaviour
 
     void Update()
     {
-        if(m_leaderID.Value == (int)NetworkManager.Singleton.LocalClientId && !m_animating)
+        if(m_leaderID.Value == (int)NetworkManager.Singleton.LocalClientId && !m_animating.Value)
         {
             var tempColor = m_background.color;
             tempColor.a = 0.0f;
@@ -31,58 +32,58 @@ public class RTSCanvas : NetworkBehaviour
             var tempColor = m_background.color;
             tempColor.a = 1.0f;
             m_background.color = tempColor;
-            m_text.text = m_textString;
+            m_text.text = m_textString.Value.ToString();
         }
     }
 
     IEnumerator RoundStartMsg()
     {
-        m_textString = "Next round's leader is " + Config.ColorNameFromPlayerID(m_leaderID.Value);
+        m_textString.Value = "Next round's leader is " + Config.ColorNameFromPlayerID(m_leaderID.Value);
         yield return new WaitForSeconds(5);
-        m_textString = "Round begins in 3";
+        m_textString.Value = "Round begins in 3";
         yield return new WaitForSeconds(1);
-        m_textString = "Round begins in 2";
+        m_textString.Value = "Round begins in 2";
         yield return new WaitForSeconds(1);
-        m_textString = "Round begins in 1";
+        m_textString.Value = "Round begins in 1";
         yield return new WaitForSeconds(1);
-        m_textString = "Follow the leader to your (invisible) spotlight!";
-        m_animating = false;
+        m_textString.Value = "Follow the leader to your (invisible) spotlight!";
+        m_animating.Value = false;
     }
     public void StartNewRound(int leaderID)
     {
         m_leaderID.Value = leaderID;
-        m_animating = true;
+        m_animating.Value = true;
         StartCoroutine("RoundStartMsg");
     }
 
     IEnumerator VictoryMsg()
     {
-        m_textString = "Nice work! You scored a point!";
+        m_textString.Value = "Nice work! You scored a point!";
         yield return new WaitForSeconds(5);
-        m_animating = false;
+        m_animating.Value = false;
     }
 
     public void RoundVictory()
     {
-        m_animating = true;
+        m_animating.Value = true;
         StartCoroutine("VictoryMsg");
     }
 
     IEnumerator FailureMsg()
     {
-        m_textString = "Time's up!";
+        m_textString.Value = "Time's up!";
         yield return new WaitForSeconds(5);
-        m_animating = false;
+        m_animating.Value = false;
     }
 
     public void RoundFailure()
     {
-        m_animating = true;
+        m_animating.Value = true;
         StartCoroutine("FailureMsg");
     }
 
     public bool IsAnimating()
     {
-        return m_animating;
+        return m_animating.Value;
     }
 }
